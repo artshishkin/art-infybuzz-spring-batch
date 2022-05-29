f31d88e07946889c6ac2d05b1c5ed6e1ea194529
Art <d.art.shishkin@gmail.com>
2022-05-29 14:34:20 +0300

    11.2 First Spring Batch Job with Tasklet Step - Testing (#1)

---
+++ getting-started/src/test/java/net/shyshkin/study/batch/gettingstarted/SampleJobTest.java
@@ -0,0 +1,43 @@
+package net.shyshkin.study.batch.gettingstarted;
+
+import net.shyshkin.study.batch.gettingstarted.config.SampleJob;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = {SampleJob.class})
+class SampleJobTest extends AbstractJobTest {
+
+    @Test
+    void sampleJobTest() throws Exception {
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        JobInstance actualJobInstance = jobExecution.getJobInstance();
+        ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+        //then
+        assertThat(actualJobInstance.getJobName()).isEqualTo("First Job");
+        assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
+
+    }
+
+    @Test
+    void firstStepTest() {
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("First Step");
+        var actualStepExecutions = jobExecution.getStepExecutions();
+        ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+        //then
+        assertThat(actualStepExecutions)
+                .hasSize(1);
+        assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
+
+    }
+}
