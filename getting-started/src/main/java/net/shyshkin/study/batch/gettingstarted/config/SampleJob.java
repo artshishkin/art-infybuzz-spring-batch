8c68b47ce7420d5af74e4469224635ea02e31c64
Art <d.art.shishkin@gmail.com>
2022-05-29 22:13:51 +0300

    19. Make Job Parameter Unique (#1)

--- getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/config/SampleJob.java
+++ getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/config/SampleJob.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +27,7 @@ public class SampleJob {
     @Bean
     Job firstJob() {
         return jobs.get("First Job")
+                .incrementer(new RunIdIncrementer())
                 .start(firstStep())
                 .next(secondStep())
                 .build();
