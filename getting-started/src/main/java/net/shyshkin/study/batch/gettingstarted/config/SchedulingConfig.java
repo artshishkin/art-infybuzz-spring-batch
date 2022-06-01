8a71bb039f4f6cae73cc6079958528aa1292cfec
Art <d.art.shishkin@gmail.com>
2022-06-01 10:20:26 +0300

    35.1 Stop Job with REST API - Implementation (#3)

--- getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/config/SchedulingConfig.java
+++ getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/config/SchedulingConfig.java
@@ -1,9 +1,11 @@
 package net.shyshkin.study.batch.gettingstarted.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@Profile("schedule")
 public class SchedulingConfig {
 }
--- getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/controller/JobController.java
+++ getting-started/src/main/java/net/shyshkin/study/batch/gettingstarted/controller/JobController.java
@@ -3,6 +3,9 @@ package net.shyshkin.study.batch.gettingstarted.controller;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.batch.gettingstarted.model.JobParamsRequest;
 import net.shyshkin.study.batch.gettingstarted.service.JobService;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class JobController {
 
     private final JobService jobService;
+    private final JobOperator jobOperator;
 
     @PostMapping("/start/{jobName}")
     public ResponseEntity<String> startJob(@PathVariable String jobName,
@@ -25,6 +29,12 @@ public class JobController {
                     return ResponseEntity.ok("Job `" + jobName + "` Started...");
                 })
                 .orElse(ResponseEntity.badRequest().body("Job with name `" + jobName + "` absent"));
+    }
+
+    @GetMapping("/stop/{jobExecutionId}")
+    public String stopJob(@PathVariable long jobExecutionId) throws NoSuchJobExecutionException, JobExecutionNotRunningException {
+        boolean stopped = jobOperator.stop(jobExecutionId);
+        return stopped ? "Job stopped..." : "Can not stop the Job...";
     }
 
 }
