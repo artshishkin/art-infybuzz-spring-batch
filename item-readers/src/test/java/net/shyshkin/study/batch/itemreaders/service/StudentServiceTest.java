878beb345ff939f9465a20af5aefa15ea114b005
Art <d.art.shishkin@gmail.com>
2022-06-03 16:49:05 +0300

    54.4 REST API as Datasource - Student Service Testing with Containers (#4)

--- item-readers/src/test/java/net/shyshkin/study/batch/itemreaders/service/StudentServiceTest.java
+++ item-readers/src/test/java/net/shyshkin/study/batch/itemreaders/service/StudentServiceTest.java
@@ -1,28 +1,44 @@
 package net.shyshkin.study.batch.itemreaders.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.batch.itemreaders.model.StudentResponse;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+@Slf4j
 @SpringBootTest
 @TestPropertySource(properties = {
-        "spring.batch.job.enabled=false"
+        "spring.batch.job.enabled=false",
+        "app.external.students-api.url=http://${SERVICES_HOST}:${SERVICES_PORT}/api/v1/"
 })
+@ContextConfiguration(initializers = StudentServiceTest.Initializer.class)
 @ActiveProfiles("external-api")
-@Disabled("Only For Manual Testing - Start `/external-api/docker-compose.yml` first")
+@Testcontainers
 class StudentServiceTest {
 
     @Autowired
     StudentService studentService;
+
+    @Container
+    protected static GenericContainer<?> studentsApiContainer = new GenericContainer<>("artarkatesoft/art-infybuzz-students-api:native")
+            .withExposedPorts(8080)
+            .waitingFor(Wait.forLogMessage(".*io.micronaut.runtime.Micronaut - Startup completed in.*\\n", 1));
 
     @Test
     void restCallToGetStudents() throws IOException, InterruptedException {
@@ -33,6 +49,21 @@ class StudentServiceTest {
         //then
         assertThat(studentResponses)
                 .hasSize(10)
-                .allSatisfy(st -> assertThat(st).hasNoNullFieldsOrProperties());
+                .allSatisfy(st -> assertAll(
+                        () -> assertThat(st).hasNoNullFieldsOrProperties(),
+                        () -> log.debug("{}", st)
+                ));
+    }
+
+    protected static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            String host = studentsApiContainer.getHost();
+            Integer port = studentsApiContainer.getMappedPort(8080);
+
+            System.setProperty("SERVICES_HOST", host);
+            System.setProperty("SERVICES_PORT", String.valueOf(port));
+        }
     }
 }
