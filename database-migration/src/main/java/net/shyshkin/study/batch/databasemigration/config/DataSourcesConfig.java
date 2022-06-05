b3041ef4e6eade20b1dbf9251f709c675777f829
Art <d.art.shishkin@gmail.com>
2022-06-05 18:40:26 +0300

    82. EntityManagerFactory For MySQL & PostgreSQL (#8)

--- database-migration/src/main/java/net/shyshkin/study/batch/databasemigration/config/DataSourcesConfig.java
+++ database-migration/src/main/java/net/shyshkin/study/batch/databasemigration/config/DataSourcesConfig.java
@@ -1,10 +1,14 @@
 package net.shyshkin.study.batch.databasemigration.config;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -33,4 +37,27 @@ public class DataSourcesConfig {
                 .create()
                 .build();
     }
+
+    @Bean
+    EntityManagerFactory posgresqlEntityManagerFactory() {
+        var lem = new LocalContainerEntityManagerFactoryBean();
+        lem.setDataSource(universityDataSource());
+        lem.setPackagesToScan("net.shyshkin.study.batch.databasemigration.posgresql.entity");
+        lem.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        lem.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        lem.afterPropertiesSet();
+        return lem.getObject();
+    }
+
+    @Bean
+    EntityManagerFactory mysqlEntityManagerFactory() {
+        var lem = new LocalContainerEntityManagerFactoryBean();
+        lem.setDataSource(destinationDataSource());
+        lem.setPackagesToScan("net.shyshkin.study.batch.databasemigration.mysql.entity");
+        lem.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        lem.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        lem.afterPropertiesSet();
+        return lem.getObject();
+    }
+
 }
