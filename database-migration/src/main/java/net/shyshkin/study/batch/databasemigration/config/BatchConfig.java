0f34544bfb0fe5470f6193c73a29f4056ab8d133
Art <d.art.shishkin@gmail.com>
2022-06-05 18:44:36 +0300

    83. Jpa Item Reader (#8)

--- database-migration/src/main/java/net/shyshkin/study/batch/databasemigration/config/BatchConfig.java
+++ database-migration/src/main/java/net/shyshkin/study/batch/databasemigration/config/BatchConfig.java
@@ -3,6 +3,7 @@ package net.shyshkin.study.batch.databasemigration.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.batch.databasemigration.posgresql.entity.Department;
+import net.shyshkin.study.batch.databasemigration.posgresql.entity.Student;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -11,11 +12,14 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Slf4j
@@ -27,6 +31,8 @@ public class BatchConfig {
     private final JobBuilderFactory jobs;
     private final StepBuilderFactory steps;
     private final DataSource universityDataSource;
+    private final EntityManagerFactory posgresqlEntityManagerFactory;
+    private final EntityManagerFactory mysqlEntityManagerFactory;
 
     @Bean
     Job chunkJob() {
@@ -38,8 +44,8 @@ public class BatchConfig {
 
     private Step chunkStep() {
         return steps.get("Just Reading Step")
-                .<Department, Department>chunk(3)
-                .reader(jdbcItemReader(-1, -1))
+                .<Student, Student>chunk(3)
+                .reader(jpaItemReader(-1, -1))
                 .writer(list -> list.forEach(item -> log.debug("{}", item)))
                 .build();
     }
@@ -58,6 +64,23 @@ public class BatchConfig {
                 .beanRowMapper(Department.class)
                 .currentItemCount(skipItems)
                 .maxRows(maxItemCount)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    JpaCursorItemReader<Student> jpaItemReader(
+            @Value("#{jobParameters['maxItemCount'] ?: -1}") int maxItemCount,
+            @Value("#{jobParameters['skipItems'] ?: 0}") int skipItems
+    ) {
+
+        return new JpaCursorItemReaderBuilder<Student>()
+                .name("jpaItemReader")
+                .entityManagerFactory(posgresqlEntityManagerFactory)
+//                .queryString("select s from Student s")
+                .queryString("from Student")
+                .currentItemCount(skipItems)
+                .maxItemCount(maxItemCount)
                 .build();
     }
 
