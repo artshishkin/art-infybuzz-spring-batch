73bf083032c4f448b7b2fc375132848555f75cba
Art <d.art.shishkin@gmail.com>
2022-06-05 10:48:29 +0300

    70.2 Capturing Bad Records While Reading Data - Test (#7)

--- fault-tolerance/src/test/java/net/shyshkin/study/batch/faulttolerance/FaultToleranceSkipJobTest.java
+++ fault-tolerance/src/test/java/net/shyshkin/study/batch/faulttolerance/FaultToleranceSkipJobTest.java
@@ -1,23 +1,38 @@
 package net.shyshkin.study.batch.faulttolerance;
 
 import net.shyshkin.study.batch.faulttolerance.model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.*;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.test.AssertFile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("fault-tolerance-skip")
 class FaultToleranceSkipJobTest extends AbstractJobTest {
 
+    public static final String SKIP_READER_OUTPUT_FILE = "../OutputFiles/Chunk Job/Fault Tolerance Step/reader/student-skip.txt";
+
     @Autowired
     FlatFileItemReader<Student> itemReader;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        Files.delete(Path.of(SKIP_READER_OUTPUT_FILE));
+    }
 
     private JobParameters defaultJobParameters() {
         JobParametersBuilder paramsBuilder = new JobParametersBuilder();
         paramsBuilder.addString("inputFile", "../InputFiles/students-fault.csv");
+        paramsBuilder.addString("skipInReaderFile", SKIP_READER_OUTPUT_FILE);
         return paramsBuilder.toJobParameters();
     }
 
@@ -32,11 +47,11 @@ class FaultToleranceSkipJobTest extends AbstractJobTest {
         //then
         assertThat(actualJobInstance.getJobName()).isEqualTo("Chunk Job");
         assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
-
+        AssertFile.assertLineCount(2, new FileSystemResource(SKIP_READER_OUTPUT_FILE));
     }
 
     @Test
-    void skipStepTest() {
+    void skipStepTest() throws Exception {
 
         //when
         JobExecution jobExecution = jobLauncherTestUtils.launchStep("Fault Tolerance Step", defaultJobParameters());
@@ -48,6 +63,6 @@ class FaultToleranceSkipJobTest extends AbstractJobTest {
                 .hasSize(1)
                 .allSatisfy(execution -> assertThat(execution.getWriteCount()).isEqualTo(8));
         assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
-
+        AssertFile.assertLineCount(2, new FileSystemResource(SKIP_READER_OUTPUT_FILE));
     }
 }
